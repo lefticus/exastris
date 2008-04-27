bca19623028e6cd980ac88ac522ed62501deb682
lefticus <lefticus@70decc49-b64b-0410-b2d8-8791ce86e832>
2008-04-27 00:53:03 +0000

    Initial GTK interface which draws current galaxy and location

--- planet.hpp
+++ planet.hpp
@@ -14,8 +14,8 @@ namespace exastris
 	: m_initial_seed(seed),
 	  m_seed(seed),
 	  m_planet_stats(Planetary_Stats(get_seed(m_initial_seed, 0), 0.0, 255.0) * ps),
-	  m_x(round(round_to(random_scalar(random_generator(m_seed), 0.0, 85.0), 5)) * 3),
-	  m_y(round(round_to(random_scalar(random_generator(m_seed), 0.0, 85.0), 5)) * 3)
+	  m_x(round(round_to(random_scalar(random_generator(m_seed), 0.0, 30.0), 5)) * 10),
+	  m_y(round(round_to(random_scalar(random_generator(m_seed), 0.0, 30.0), 5)) * 10)
       {
       }
 
@@ -24,7 +24,7 @@ namespace exastris
 	return round(sqrt( pow(fabs(p.m_x - m_x), 2) + pow(fabs(p.m_y - m_y), 2)));
       }
 
-      unsigned int m_initial_seed;
+      const unsigned int m_initial_seed;
       unsigned int m_seed;
 
       const Planetary_Stats m_planet_stats;
